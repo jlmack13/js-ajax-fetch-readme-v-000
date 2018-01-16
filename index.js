1a842259023a635544aab6b818387dd14250348a
jlmack13 <jennimacklin@gmail.com>
2018-01-16 16:04:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+//
+// fetch('https://api.github.com/repos/jquery/jquery/commits')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+//
+// fetch('https://api.github.com/user/repos').
+//   then(res => res.json()).
+//   then(json => console.log(json))
+
+const token = 'TOKEN-HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => json.()).then(json => console.log(json));
